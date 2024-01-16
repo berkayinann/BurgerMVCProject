f3875c5324e4c058be7afbb0ecee699e6eaf138f
Tarık Çelebi <trk_208@hotmail.com>
2024-01-16 16:57:19 +0300

    services fixed and program.cs in UI arrenged.

--- BurgerMVCProject.BLL/Services/Abstract/IExtraProductService.cs
+++ BurgerMVCProject.BLL/Services/Abstract/IExtraProductService.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using BurgerMVCProject.Domain.Entities;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +9,9 @@ namespace BurgerMVCProject.BLL.Services.Abstract
 {
     public interface IExtraProductService
     {
+        List<ExtraProduct> GetEProducts();
+        bool AddEProduct(ExtraProduct extraProduct);
+        bool UpdateEProduct(ExtraProduct extraProduct);
+        bool DeleteEProduct(ExtraProduct extraProduct);
     }
 }
--- BurgerMVCProject.BLL/Services/Concrete/ExtraProductService.cs
+++ BurgerMVCProject.BLL/Services/Concrete/ExtraProductService.cs
@@ -1,4 +1,7 @@
-﻿using DAL.Repositories.Abstract;
+﻿using BurgerMVCProject.BLL.Services.Abstract;
+using BurgerMVCProject.Domain.Entities;
+using DAL.Repositories.Abstract;
+using DAL.Repositories.Concrete;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -7,12 +10,71 @@ using System.Threading.Tasks;
 
 namespace BurgerMVCProject.BLL.Services.Concrete
 {
-    public class ExtraProductService
+    public class ExtraProductService : IExtraProductService
     {
-        private readonly IExtraProductRepository repository;
-        public ExtraProductService(IExtraProductRepository repository)
+        private readonly IExtraProductRepository EProductRepository;
+
+        public ExtraProductService(IExtraProductRepository EProductrepository)
         {
-            this.repository = repository;
+            this.EProductRepository = EProductrepository;
         }
+
+        public bool AddEProduct(ExtraProduct extraProduct)
+        {
+            var file = extraProduct.Image;
+            string path = Path.Combine(Directory.GetCurrentDirectory(), @"wwwroot\\MenuPhotos", file.FileName);
+
+            using (var fileStream = new FileStream(path, FileMode.Create))
+            {
+                file.CopyToAsync(fileStream);
+            }
+
+            ExtraProduct eProduct = new()
+            {
+                Name = extraProduct.Name,
+                Price = extraProduct.Price,
+                CreatedDate = DateTime.Now,
+                ImageSrc = path
+            };
+
+            return EProductRepository.Add(eProduct);
+        }
+
+        public bool UpdateEProduct(ExtraProduct extraProduct)
+        {
+            var file = extraProduct.Image;
+            string path = Path.Combine(Directory.GetCurrentDirectory(), @"wwwroot\MenuPhotos", file.FileName);
+
+            using (var fileStream = new FileStream(path, FileMode.Create))
+            {
+                file.CopyToAsync(fileStream);
+            }
+
+            ExtraProduct eProduct = new()
+            {
+                Name = extraProduct.Name,
+                Price = extraProduct.Price,
+                CreatedDate = DateTime.Now,
+                ImageSrc = path
+            };
+            return EProductRepository.Update(eProduct);
+        }
+
+        public bool DeleteEProduct(ExtraProduct extraProduct)
+        {
+            return EProductRepository.Delete(extraProduct);
+        }
+
+        public List<ExtraProduct> GetEProducts()
+        {
+            return EProductRepository.GetAll().Select(s => new ExtraProduct()
+            {
+                MenuId = s.MenuId,
+                Name = s.Name,
+                ImageSrc = s.ImageSrc,
+                Price = s.Price
+            }).ToList();
+        }
+
     }
 }
