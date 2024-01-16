e4d5c45db0bcd7b87a4c308a1ed27306a6e4c6e7
Tarık Çelebi <trk_208@hotmail.com>
2024-01-16 19:02:31 +0300

    clearedIssues-layoutEdited

--- BurgerMVCProject.Domain/Entities/ExtraProduct.cs
+++ BurgerMVCProject.Domain/Entities/ExtraProduct.cs
@@ -9,10 +9,7 @@ namespace BurgerMVCProject.Domain.Entities
         public int MenuId { get; set; }
 
         public Menu Menu { get; set; }
-
-        public int CategoryId { get; set; }
-
-        public Category Category { get; set; }
+        public Enums.Category Category { get; set; }
 
         public int Quantity { get; set; }
 
