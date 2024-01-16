8cd686f4290f14fdd54102f9c66b6fa3ae6079b6
Berkay INAN <inan.berkay@outlook.com>
2024-01-16 16:51:24 +0300

    feature-configurations-appuser

---
+++ BurgerMVCProject.DAL/Configurations/ExtraProductConfiguration.cs
@@ -0,0 +1,19 @@
+﻿using BurgerMVCProject.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL.Configurations
+{
+    public class ExtraProductConfiguration : IEntityTypeConfiguration<ExtraProduct>
+    {
+        public void Configure(EntityTypeBuilder<ExtraProduct> builder)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ BurgerMVCProject.DAL/Configurations/OrderConfiguration.cs
@@ -0,0 +1,23 @@
+﻿using BurgerMVCProject.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL.Configurations
+{
+    public class OrderConfiguration : IEntityTypeConfiguration<Order>
+    {
+        public void Configure(EntityTypeBuilder<Order> builder)
+        {
+            builder.HasKey(x => x.OrderId);
+            builder.ToTable("Orders");
+            builder.HasOne(o => o.AppUser)
+                   .WithMany(u => u.Orders)
+                   .HasForeignKey(o => o.AppUserId);
+        }
+    }
+}
