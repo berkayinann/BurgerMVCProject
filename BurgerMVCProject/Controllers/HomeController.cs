3de4064c1826d4e5300bd3a10b12bb3b9d7f64ce
Tarık Çelebi <trk_208@hotmail.com>
2024-01-17 16:47:04 +0300

    home and the order controller created and cshtmls created.

--- BurgerMVCProject/Controllers/HomeController.cs
+++ BurgerMVCProject/Controllers/HomeController.cs
@@ -1,3 +1,5 @@
+﻿using BurgerMVCProject.BLL.Services.Abstract;
+using BurgerMVCProject.Domain.Entities;
 using BurgerMVCProject.Models;
 using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
@@ -6,10 +8,43 @@ namespace BurgerMVCProject.Controllers
 {
     public class HomeController : Controller
     {
+        private readonly IExtraProductService eProductService;
+        private readonly IMenuService menuService;
+        private readonly IOrderService orderService;
+
         public IActionResult Index()
         {
-            return View();
+            List<Menu> menuList = new List<Menu>()
+            {
+                new Menu()
+                {
+                    MenuId=1,
+                    Name="Burger",
+                    Description="cheddarlı",
+                    ImageSrc="~/MenuPhotos/b3.png"
+                    
+                   
+                },
+                new Menu()
+                {
+                    MenuId=2,
+                    Name="Burger",
+                    Description="cheddarlı",
+                    ImageSrc="~/MenuPhotos/b3.png"
+                },
+                new Menu()
+                {
+                    MenuId=3,
+                    Name="Burger",
+                    Description="çift kaşarlı",
+                    ImageSrc="~/MenuPhotos/b3.png"
+                },
+
+            };
+            return View(menuList);
         }
+
+
 
         public IActionResult Privacy()
         {
