332c50cb96d913f30ac4a875e5ae528d3bc597c0
fatihkabalcioglu <onlykabalci@gmail.com>
2024-01-17 16:45:47 +0300

    menu&extraProduct controller added

--- BurgerMVCProject.BLL/Services/Concrete/MenuService.cs
+++ BurgerMVCProject.BLL/Services/Concrete/MenuService.cs
@@ -79,6 +79,5 @@ namespace BurgerMVCProject.BLL.Services.Concrete
         {
             return menuRepository.GetById(id);
         }
-
     }
 }
---
+++ BurgerMVCProject/Controllers/ExtraProductController.cs
@@ -0,0 +1,53 @@
+﻿using BurgerMVCProject.BLL.Services.Abstract;
+using BurgerMVCProject.Domain.Entities;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BurgerMVCProject.UI.Controllers
+{
+    public class ExtraProductController : Controller
+    {
+        private readonly IExtraProductService extraProductService;
+        public ExtraProductController(IExtraProductService extraProductService)
+        {
+            this.extraProductService = extraProductService;
+        }
+
+        public IActionResult Index()
+        {
+            List<ExtraProduct> extraProducts = extraProductService.GetEProducts();
+            return View();
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(ExtraProduct extraProduct)
+        {
+            extraProductService.AddEProduct(extraProduct);
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult Update(int eProductId)
+        {
+            extraProductService.GetByIdProduct(eProductId);
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Update(ExtraProduct extraProduct)
+        {
+            extraProductService.UpdateEProduct(extraProduct);
+            return RedirectToAction("Index");
+        }
+
+        public IActionResult Delete(int eProductId)
+        {
+            ExtraProduct extraProduct = extraProductService.GetByIdProduct(eProductId);
+            extraProductService.DeleteEProduct(extraProduct);
+            return RedirectToAction("Index");
+        }
+    }
+}
